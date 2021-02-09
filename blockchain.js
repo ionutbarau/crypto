5c92b95d9c841cda8dcf2897453ce6e1ab2208c3
ionut <ionutbarau@gmail.com>
2021-02-09 19:36:14 +0200

    Finished blockchain backend (section 3)

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const cryptoHash = require('./crypto-hash');
 
 class Blockchain{
     constructor() {
@@ -11,6 +12,42 @@ class Blockchain{
             data
         })
         this.chain.push(newBlock);
+    }
+
+    replaceChain(chain){
+        if(chain.length <= this.chain.length){
+            console.error('The incoming chain must be longer');
+            return;
+        }
+        if(!Blockchain.isValidChain(chain)){
+            console.error('The incoming chain must be valid');
+            return;
+        }
+        console.log('Replacing chain with', chain);
+        this.chain = chain;
+    }
+
+    static isValidChain(chain){
+        //check if it has genesis as first block
+        if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())){
+            return false;
+        }
+
+        for(let i= 1; i<chain.length; i++){
+            const {timestamp, lastHash, hash, data} = chain[i];
+            const previousBlock = chain[i-1];
+
+            //current block's lastHash has to match
+            if(lastHash !== previousBlock.hash){
+                return false;
+            }
+            //current block's hash has to match with the one generated from the current block's fields
+            const validatedHash = cryptoHash(timestamp, lastHash, data);
+            if(hash !== validatedHash){
+                return false;
+            }
+        }
+        return true;
     }
 }
 

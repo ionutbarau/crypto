b92760be08aff6f39b703313dcba35546f27aa98
ionut <ionutbarau@gmail.com>
2021-02-25 23:20:34 +0200

    Finished frontend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const request = require('request');
+const path = require('path');
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
 const TransactionPool = require('./wallet/transaction-pool')
@@ -19,6 +20,7 @@ const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'client/dist')));
 
 app.get('/api/blocks',(req, res) =>{
         res.json(blockchain.chain);
@@ -66,6 +68,10 @@ app.get('/api/wallet-info',(req, res) =>{
    })
 });
 
+app.get('*', (req, res) =>{
+   res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+});
+
 const syncWithRootState = () =>{
    request({url:`${ROOT_NODE_ADDRESS}/api/blocks`}, (error, response, body) =>{
       if(!error && response.statusCode === 200){
@@ -82,6 +88,36 @@ const syncWithRootState = () =>{
          transactionPool.setMap(rootTransactionPoolMap);
       }
    });
+}
+
+
+const walletFoo = new Wallet();
+const walletBar = new Wallet();
+
+const generateWalletTransaction = ({wallet, recipient, amount}) =>{
+   const transaction = wallet.createTransaction({recipient, amount, chain:blockchain.chain});
+   transactionPool.setTransaction(transaction);
+}
+
+const walletAction = () => generateWalletTransaction({wallet, recipient:walletFoo.publicKey, amount:5});
+
+const walletFooAction = () => generateWalletTransaction({wallet:walletFoo, recipient:walletBar.publicKey, amount:10});
+
+const walletBarAction = () => generateWalletTransaction({wallet:walletBar, recipient:wallet.publicKey, amount:15});
+
+for(let i=0;i<10;i++){
+   if(i%3 === 0){
+      walletAction();
+      walletFooAction();
+   }else if(i%3===1){
+      walletAction();
+      walletBarAction();
+   }else{
+      walletFooAction();
+      walletBarAction();
+   }
+
+   transactionMiner.mineTransactions();
 }
 
 

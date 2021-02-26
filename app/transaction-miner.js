041133eb41ecfd13935ffefffadc0c1a2f302c04
ionut <ionutbarau@gmail.com>
2021-02-26 15:40:33 +0200

    Fixed mining transactions when pool is empty

--- app/transaction-miner.js
+++ app/transaction-miner.js
@@ -12,14 +12,16 @@ class TransactionMiner {
     mineTransactions() {
         //get the transaction pool's valid transactions
         const validTransactions = this.transactionPool.validTransactions();
-        //generate the miner's reward
-        validTransactions.push(Transaction.rewardTransaction({minerWallet:this.wallet}));
-        //add a block consisting of these transactions to the blockchain
-        this.blockchain.addBlock({data:validTransactions});
-        //broadcast the updated blockchain
-        this.pubsub.broadcastChain();
-        //clear the pool
-        this.transactionPool.clear();
+        if(validTransactions.length > 0) {
+            //generate the miner's reward
+            validTransactions.push(Transaction.rewardTransaction({minerWallet: this.wallet}));
+            //add a block consisting of these transactions to the blockchain
+            this.blockchain.addBlock({data: validTransactions});
+            //broadcast the updated blockchain
+            this.pubsub.broadcastChain();
+            //clear the pool
+            this.transactionPool.clear();
+        }
     }
 }
 
